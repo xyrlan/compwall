148460733cb524e4150b31c0a17ac3c12f98cecb
xyrlan <lobatopedro68@gmail.com>
2024-07-19 17:04:35 -0400

    update

--- app/page.tsx
+++ app/page.tsx
@@ -7,12 +7,12 @@ import TabsComponent from "@/components/tabs-component";
 export default function Home() {
   return (
     <section className="flex flex-col gap-4">
-      <BreadcrumbsComponent items={[{ name: "Production", href: "/" }, { name: "Visao Geral", href: "/" }]} />
+      <BreadcrumbsComponent items={[{ name: "Production", href: "/" }, { name: "Visão Geral", href: "/" }]} />
       <TabsComponent items={[{
         name: "Dataflow", key: "dataflow", content: <div className="h-[900px]">
           <Flow />
         </div>
-      }, { name: "Aplicacoes", key: "apps", content: <div>Aplicacoes</div> }]} />
+      }, { name: "Log de aplicações", key: "apps", content: <div>Aplicacoes</div> }]} />
     </section>
   );
 }
